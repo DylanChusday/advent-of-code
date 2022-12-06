2fea01a89d761b254cec57dbb0fea5bc35ba0065
Chusday <dylanc.chu@gmail.com>
2022-12-06 08:25:13 -0500

    AoC day 6

---
+++ day6/day6p1.py
@@ -0,0 +1,18 @@
+processedChars = 0
+flag = 0
+with open('day6input.txt') as file:
+    for line in file.readlines():
+        databuffer = []
+        for x in line:
+            if flag ==0:
+                if len(databuffer) < 4:
+                    databuffer.append(x)
+                    processedChars += 1
+                else:
+                    databuffer.pop(0)
+                    databuffer.append(x)
+                    processedChars += 1
+                    if len(databuffer) == len(set(databuffer)):
+                        flag = 1
+
+print(processedChars)
